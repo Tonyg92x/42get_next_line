20da1d7946c836f3a13a656d18a2c99dc5f1f27a
Anthony Guay <aguay@c2r2p12.42quebec.com>
2021-12-13 13:04:06 -0500

    name change

---
+++ TonyG_gnl/srcs/get_next_line_bonus.h
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_bonus.h                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aguay <aguay@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/11/24 09:19:19 by aguay             #+#    #+#             */
+/*   Updated: 2021/12/09 14:54:29 by aguay            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_BONUS_H
+# define GET_NEXT_LINE_BONUS_H
+
+# include <stdio.h>
+# include <stdbool.h>
+# include <unistd.h>
+# include <stdlib.h>
+# include <limits.h>
+# include <sys/stat.h>
+# include <sys/types.h>
+# include <fcntl.h>
+
+char	*get_next_line(int fd);
+char	*ft_get(char *buffer, int *len, int fd, char *retour);
+bool	ft_protection(char **buffer, char **retour, int *count_retour);
+#endif
